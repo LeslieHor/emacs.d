b03fff7e2463cced39986f33434e7369f5319117
leslie <leslie.hor@gmail.com>
2020-02-19 20:21:20 +0000

    optional erlang

--- init.el
+++ init.el
@@ -1,3 +1,3 @@
 (setq vc-follow-symlinks t) ;; To disable prompt on start up when using symlinks
-(org-babel-load-file "~/.emacs.d/configuration.org")
 (org-babel-load-file "~/.emacs.d/personalsettings.org")
+(org-babel-load-file "~/.emacs.d/configuration.org")
