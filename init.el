9db8799db301affd5bdda4e59792b761968e68e1
leslie <leslie.hor@gmail.com>
2020-06-26 21:23:33 +0100

    make notifications available in termux

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (setq vc-follow-symlinks t) ;; To disable prompt on start up when using symlinks
 (put 'narrow-to-region 'disabled nil)
-(setq safe-local-variable-values '((eval leslie/file-local-eval-safe-auto-revert)))
+(setq safe-local-variable-values '((eval circ/file-local-eval-safe-auto-revert)))
 (org-babel-load-file "~/.emacs.d/personalsettings.org")
 (org-babel-load-file "~/.emacs.d/settings.org")
 (org-babel-load-file "~/.emacs.d/configuration.org")
