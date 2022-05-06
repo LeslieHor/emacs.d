410656fda649d49ae322213817df768e1a23e85a
Leslie Hor <Leslie.Hor@Gmail.com>
2022-05-07 00:06:08 +0100

    Implement eglot

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+;; Disable activation of installed packages to force using manually updated
+;; versions of built-in packages
+(package-initialize nil)
+(setq package-enable-at-startup nil)
+(add-to-list 'load-path "~/.emacs.d/packages/eldoc-1.11.1")
+(add-to-list 'load-path "~/.emacs.d/packages/flymake-1.2.2")
+(add-to-list 'load-path "~/.emacs.d/packages/jsonrpc-1.0.15")
+(add-to-list 'load-path "~/.emacs.d/packages/project-0.8.1")
+(add-to-list 'load-path "~/.emacs.d/packages/seq-2.23")
+(add-to-list 'load-path "~/.emacs.d/packages/xref-1.0.1")
+(package-initialize t)
 (setq vc-follow-symlinks t) ;; To disable prompt on start up when using symlinks
 (put 'narrow-to-region 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
